c8d9b1b8ff28ba788825916a9c0907ea509c18fc
Leonid <elieyzer1@gmail.com>
2025-01-23 00:51:53 +0200

    2 mavoa

---
+++ 3_SugeModulim/try_2.js
@@ -0,0 +1,27 @@
+/* На изображении приведён пример использования стороннего модуля mathjs в Node.js. Вот подробное объяснение, как работать с этим примером:
+Шаги:
+1. Инициализация проекта
+Создайте package.json для управления зависимостями. флаг -y создаёт файл package.json с настройками по умолчанию.:
+npm init -y
+Установите библиотеку mathjs: Это добавит mathjs в список зависимостей в вашем package.json и создаст папку node_modules.
+npm install mathjs
+
+node try_2  
+
+
+*/
+const math = require('mathjs'); // Подключаем модуль mathjs
+
+// Пример использования математических функций
+const a = 3;
+const b = 4;
+
+const sum = math.add(a, b);
+const product = math.multiply(a, b);
+const power = math.pow(a, b);
+const sqrt = math.sqrt(b);
+
+console.log(`Sum of ${a} and ${b} is:`, sum);
+console.log(`Product of ${a} and ${b} is:`, product);
+console.log(`${a} raised to the power of ${b} is:`, power);
+console.log(`Square root of ${b} is:`, sqrt);
---
+++ 3_SugeModulim/try_3a.js
@@ -0,0 +1,5 @@
+// Импортируем локальный модуль
+const sayHello = require('./try_3.js');
+
+// Вызываем функцию из модуля
+sayHello(); // Выведет: Hello, world!
