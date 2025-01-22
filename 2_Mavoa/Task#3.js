cac805b639bb2b003de733dfa2d51f5004fc2b95
Leonid <elieyzer1@gmail.com>
2025-01-23 00:27:52 +0200

    Добавлены все файлы проекта

---
+++ 2_Mavoa/Task#3.js
@@ -0,0 +1,8 @@
+// Исходный массив чисел
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Функция для вычисления количества четных чисел
+const countEvenNumbers = numbers.reduce((count, num) => count + (num % 2 === 0 ? 1 : 0), 0);
+
+// Вывод результата
+console.log(`Количество четных чисел: ${countEvenNumbers}`);
